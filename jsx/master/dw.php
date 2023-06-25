31c8c555f6573a358271451ee1cf1e8f95eee801
mardiyanto <mardybest@gmail.com>
2023-06-25 20:13:07 +0700

    update dw.php

--- jsx/master/dw.php
+++ jsx/master/dw.php
@@ -13,7 +13,7 @@ $tanggal=date("dmYhis");
 $file=$_FILES['gb']['tmp_name'];
 $file_name=$_FILES['gb']['name'];
 copy($file,"../../foto/admin/".$tanggal.".jpg");
-mysql_query("insert into kat_download (kategori,gb) values ('$_POST[kat]','$tanggal.jpg')");  
+mysqli_query($koneksi,"insert into kat_download (kategori,gb) values ('$_POST[kat]','$tanggal.jpg')");  
 echo "<script>window.location=('../index.php?aksi=kat_d')</script>";
 
   } 
@@ -29,7 +29,7 @@ if (empty($_POST[kat])){
 $lokasi_file=$_FILES[gb][tmp_name];
 if(empty($lokasi_file)){
 
-mysql_query("UPDATE kat_download SET kategori='$_POST[kat]', gb='$_POST[gb]' WHERE no='$_GET[id_d]'");
+mysqli_query($koneksi,"UPDATE kat_download SET kategori='$_POST[kat]', gb='$_POST[gb]' WHERE no='$_GET[id_d]'");
 echo "<script>window.location=('../index.php?aksi=kat_d')</script>";
 
 }else{
@@ -38,7 +38,7 @@ $tanggal=date("dmYhis");
 $file=$_FILES['gb']['tmp_name'];
 $file_name=$_FILES['gb']['name'];
 copy($file,"../../foto/admin/".$tanggal.".jpg");
-mysql_query("UPDATE kat_download SET kategori='$_POST[kat]', gb='$tanggal.jpg'WHERE no='$_GET[id_d]'");
+mysqli_query($koneksi,"UPDATE kat_download SET kategori='$_POST[kat]', gb='$tanggal.jpg'WHERE no='$_GET[id_d]'");
 
 echo "<script>window.location=('../index.php?aksi=kat_d')</script>";
 }else{
@@ -48,29 +48,17 @@ $a=$_GET['gb'];
 $file=$_FILES['gb']['tmp_name'];
 $file_name=$_FILES['gb']['name'];
 copy($file,"../../foto/admin/".$a);
-mysql_query("UPDATE kat_download SET kategori='$_POST[kat]' WHERE no='$_GET[id_d]'");
+mysqli_query($koneksi,"UPDATE kat_download SET kategori='$_POST[kat]' WHERE no='$_GET[id_d]'");
 echo "<script>window.location=('../index.php?aksi=kat_d')</script>";
     }
    }
   } 
 }
-
-
-
-
-
-
-
-
-
 elseif($act=='hapus'){
-mysql_query("DELETE FROM kat_download WHERE no='$_GET[id_d]'");
+mysqli_query($koneksi,"DELETE FROM kat_download WHERE no='$_GET[id_d]'");
 $b=$_GET['gb'];
 $pathFile="../../foto/admin/$b";	   
 unlink($pathFile);
 echo "<script>window.location=('../index.php?aksi=kat_d')</script>";
 }
-
-
-
 ?>
