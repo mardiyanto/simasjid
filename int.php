e115d631c4b94965c69a7f18190a8b2d6b7bf5e5
mardiyanto <mardybest@gmail.com>
2023-06-25 20:32:08 +0700

    fix

--- int.php
+++ int.php
@@ -373,7 +373,7 @@ elseif ($_GET[module]=='galery'){ ?>
   <div class="container-xxl py-5">
             <div class="container">
                 <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <h6 class="section-title text-center text-primary text-uppercase">iNFORMASI UPDATE</h6>
+           
                     <h1 class="mb-5">INFORMASI <span class="text-primary text-uppercase">TERKINI</span></h1>
                 </div>
                 <div class="row g-4">
